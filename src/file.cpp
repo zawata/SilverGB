dd41d95170963316ec5bc73eb603fa2db572447f
John Alden <jzalden96@gmail.com>
2018-08-21 22:00:33 -0700

    add output capabilities to the file class

--- src/file.cpp
+++ src/file.cpp
@@ -4,10 +4,13 @@ File_Interface::File_Interface() {}
 
 File_Interface::~File_Interface() {}
 
-File_Interface *File_Interface::openFile(std::string filename) {
+File_Interface *File_Interface::openFile(std::string filename, bool write) {
     auto ret = new File_Interface();
 
-    if(ret->file = std::ifstream(filename, std::ifstream::in | std::ifstream::binary)) {
+    if(ret->file = std::fstream(filename,
+            std::ifstream::in |
+            std::ifstream::out |
+            std::ifstream::binary)) {
         return ret;
     } else {
         delete ret;
@@ -15,17 +18,31 @@ File_Interface *File_Interface::openFile(std::string filename) {
     }
 }
 
-void File_Interface::seekFile(u32 offset) {
+void File_Interface::seekFile_g(u32 offset) {
     file.seekg(offset, std::ios_base::beg);
 }
 
+void File_Interface::seekFile_p(u32 offset) {
+    file.seekp(offset, std::ios_base::beg);
+}
+
 u8 File_Interface::getByte(u32 offset) {
-    seekFile(offset);
+    seekFile_g(offset);
     return file.get();
 }
 
 size_t File_Interface::getBuffer(u32 offset, u8 *buf, size_t len) {
-    seekFile(offset);
+    seekFile_g(offset);
     file.get((char *)buf, len);
     return this->file.gcount();
+}
+
+void File_Interface::setByte(u32 offset, u8 data) {
+    seekFile_p(offset);
+    file.put(data);
+}
+
+void File_Interface::setBuffer(u32 offset, u8 *buf, size_t len) {
+    seekFile_g(offset);
+    file.write((char *)buf, len);
 }
--- src/file.hpp
+++ src/file.hpp
@@ -1,26 +1,27 @@
+#pragma once
+
 #include <fstream>
 
 #include "util.hpp"
 
-#ifndef FILE_H
-#define FILE_H
-
 class File_Interface {
 public:
-    
+
     ~File_Interface();
 
-    static File_Interface *openFile(std::string filename);
-
-    void seekFile(u32 offset);
+    static File_Interface *openFile(std::string filename, bool write=false);
 
     u8 getByte(u32 offset);
     size_t getBuffer(u32 offset, u8 *buf, size_t len);
 
+    void setByte(u32 offset, u8 data);
+    void setBuffer(u32 offset, u8 *buf, size_t len);
+
 private:
+    std::fstream file;
+
     File_Interface();
 
-    std::ifstream file;
+    void seekFile_g(u32 offset);
+    void seekFile_p(u32 offset);
 };
-
-#endif
