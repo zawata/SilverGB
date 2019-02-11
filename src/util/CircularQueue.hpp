6f82206f30e0c3bd633340625a68e84f4b9ff8de
John Alden <jzalden96@gmail.com>
2019-02-11 14:25:10 -0700

    fix CircularQueue Class
    because of template compilation stuff, Circular Queue has to be a header file.

--- src/util/CircularQueue.hpp
+++ src/util/CircularQueue.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
-#include "ints.hpp"
+#include <cstdlib>
+#include <cstring>
+
+#include "util/ints.hpp"
 
 template<typename T>
 class CircularQueue {
@@ -16,3 +19,55 @@ public:
 private:
     T *start, *head, *tail, *end;
 };
+
+template<typename T>
+CircularQueue<T>::CircularQueue(size_t size):
+start(static_cast<T *>(malloc(sizeof(T) * size))),
+head(start),
+tail(start),
+end(start+(sizeof(T) * size)) {}
+
+template<typename T>
+bool CircularQueue<T>::enqueue(T elem) {
+    if((tail+1) != head) {
+        tail++;
+
+        if(tail == end) {
+            if(head == start) {
+                tail--;
+                return false;
+            }
+            else tail = start;
+        }
+
+        *tail = elem;
+        return true;
+    }
+    else return false;
+}
+
+template<typename T>
+T CircularQueue<T>::dequeue(void) {
+    T retval;
+    if(head != tail) {
+        head++;
+        if(head == end) head = start;
+
+        retval = *head;
+
+        return retval;
+    }
+    else return (T)0;
+}
+
+template<typename T>
+size_t CircularQueue<T>::size(void) {
+    if      (head <  tail) return tail - head;
+    else if (head >  tail) return (end-start)-(head-tail);
+    else                   return 0;
+}
+
+template<typename T>
+void CircularQueue<T>::clear(void) {
+    head = tail = start;
+}
