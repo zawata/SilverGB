02f43019f3a07c718cec578722255cf7a43ccdb2
John Alden <john.alden@gitkraken.com>
2022-05-10 12:03:06 -0700

    major CPU overhaul. still broken though

--- src/gba_core/cpu.hpp
+++ src/gba_core/cpu.hpp
@@ -14,12 +14,12 @@ public:
     CPU(IO_Bus *io, bool bootrom_enabled = false);
     ~CPU();
 
+    void tick();
+
     u8 decode(u32 op);
     u8 decode_thumb(u16 op);
 private:
     IO_Bus *io;
-
-    void tick();
 
     enum op_type_t {
         op_type_REG,
@@ -36,21 +36,37 @@ private:
 
     enum op_data_proc_cmd_t {
         op_data_proc_cmd_AND, // binary and
+        op_data_proc_cmd_ANDS,
         op_data_proc_cmd_EOR, // binary exclusive or
+        op_data_proc_cmd_EORS,
         op_data_proc_cmd_SUB, // arithemetic subtract
+        op_data_proc_cmd_SUBS,
         op_data_proc_cmd_RSB, // arithmetic reverse substract
+        op_data_proc_cmd_RSBS,
         op_data_proc_cmd_ADD, // arithemetic add
+        op_data_proc_cmd_ADDS,
         op_data_proc_cmd_ADC, // arithemetic add with carry
+        op_data_proc_cmd_ADCS,
         op_data_proc_cmd_SBC, // arithemetic subtract with carry
+        op_data_proc_cmd_SBCS,
         op_data_proc_cmd_RSC, // arithemetic reverse subtract with carry
+        op_data_proc_cmd_RSCS,
+        op_data_proc_cmd_noset1,
         op_data_proc_cmd_TST, // test and
+        op_data_proc_cmd_noset2,
         op_data_proc_cmd_TEQ, // test exclusive or
-        op_data_proc_cmd_CMP, // test compare(subtract)
-        op_data_proc_cmd_CMN, // test compare(add)
+        op_data_proc_cmd_noset3,
+        op_data_proc_cmd_CMP,// test compare(subtract)
+        op_data_proc_cmd_noset4,
+        op_data_proc_cmd_CMN,// test compare(add)
         op_data_proc_cmd_ORR, // binary or
+        op_data_proc_cmd_ORRS,
         op_data_proc_cmd_MOV, // move
+        op_data_proc_cmd_MOVS,
         op_data_proc_cmd_BIC, // binary bit clear
+        op_data_proc_cmd_BICS,
         op_data_proc_cmd_MVN, // move not
+        op_data_proc_cmd_MVNS,
     };
 
     enum op_data_proc_shift_t {
@@ -58,12 +74,6 @@ private:
         op_data_proc_shift_LSR,
         op_data_proc_shift_ASR,
         op_data_proc_shift_ROR
-    };
-
-    enum op_cycle_t {
-        op_cycle_CALC_B_DEST, // alu = PC + imm
-        op_cycle_FETCH_ALU_AND_LINK,
-        op_cycle_WRITE_LR,
     };
 
     u32 op1;
@@ -74,13 +84,41 @@ private:
     void execute();
 
     u32 calc_shift_operand(op_data_proc_shift_t shift_type, u8 shift_amount, u8 reg_M, bool *carry_out = nullptr);
-    void execute_data_processing_command(CPU::op_data_proc_cmd_t command, bool set_flags, u8 reg_N, u8 reg_D, u32 shift_operand, bool shift_carry_out);
+
+    //op decoders
+    void decode_dp_op(u32 word);
+    void decode_mul_op(u32 word);
+    void decode_load_store_op(u32 word);
+    void decode_coproc_op(u32 word);
+    void decode_branch_op(u32 word);
+    void decode_bex_op(u32 word);
+    void decode_swap_op(u32 word);
+    void decode_swi(u32 word);
+
+    //op interpreters
+    void execute_data_processing_command(
+        CPU::op_data_proc_cmd_t command,
+        u8 reg_N,
+        u8 reg_D,
+        u32 shift_operand,
+        bool shift_carry_out
+    );
+
+    void execute_load_store_command(
+        bool load,
+        bool pre_index,
+        bool add,
+        bool word,
+        bool write,
+        u8 reg_N,
+        u8 reg_D,
+        u32 offset
+    );
 
     //op implementations
     void data_proc_reg(
-        bool condition,
+        bool (*condition)(void),
         op_data_proc_cmd_t command,
-        bool set_flags,
         u8 reg_N,
         u8 reg_D,
         u8 shift_amount,
@@ -89,9 +127,8 @@ private:
     );
 
     void data_proc_shifted_reg(
-        bool condition,
+        bool (*condition)(void),
         op_data_proc_cmd_t command,
-        bool set_flags,
         u8 reg_N,
         u8 reg_D,
         u8 shift_amount,
@@ -100,22 +137,20 @@ private:
     );
 
     void data_proc_immediate(
-        bool condition,
+        bool (*condition)(void),
         op_data_proc_cmd_t command,
-        bool set_flags,
         u8 reg_N,
         u8 reg_D,
         u8 rotate,
         u8 immediate
     );
 
-    void load_store_shifted_reg(
-        bool condition,
+    void load_shifted_reg(
+        bool (*condition)(void),
         bool pre_index,
         bool add,
         bool word,
         bool write,
-        bool is_load,
         u8 reg_N,
         u8 reg_D,
         u8 shift_amount,
@@ -123,17 +158,49 @@ private:
         u8 reg_M
     );
 
-    void load_store_immediate(
-        bool condition,
+    void store_shifted_reg(
+        bool (*condition)(void),
         bool pre_index,
         bool add,
         bool word,
         bool write,
-        bool is_load,
+        u8 reg_N,
+        u8 reg_D,
+        u8 shift_amount,
+        op_data_proc_shift_t shift_type,
+        u8 reg_M
+    );
+
+    void load_immediate(
+        bool (*condition)(void),
+        bool pre_index,
+        bool add,
+        bool word,
+        bool write,
         u8 reg_N,
         u8 reg_D,
         u16 immediate
     );
 
-    void branch(bool condition, bool link, s32 immediate);
+    void store_immediate(
+        bool (*condition)(void),
+        bool pre_index,
+        bool add,
+        bool word,
+        bool write,
+        u8 reg_N,
+        u8 reg_D,
+        u16 immediate
+    );
+
+    void branch(
+        bool (*condition)(void),
+        bool link,
+        s32 immediate
+    );
+
+    void branch_and_exchange(
+        bool (*condition)(void),
+        s32 immediate
+    );
 };
--- src/gba_core/main.cpp
+++ src/gba_core/main.cpp
@@ -3,10 +3,15 @@
 #include "gba_core/cart.hpp"
 #include "gba_core/cpu.hpp"
 #include "gba_core/io.hpp"
+#include <cstdio>
 
 int main() {
-    Silver::File *file = Silver::File::openFile(R"u8(D:/Pokemon - Ruby Version (USA).gba)u8");
+    Silver::File *file = Silver::File::openFile("../test_files/gba_tests/pokemon_ruby.gba");
     Cartridge *cart = new Cartridge(file);
     IO_Bus *io = new IO_Bus(cart);
     CPU *cpu = new CPU(io);
+
+    while(getchar() == '\n') {
+        cpu->tick();
+    }
 }
