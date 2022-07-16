b02cf5d2ba3272ef2b7ef19053ad5961b34ea9f2
John Alden <john.alden@gitkraken.com>
2022-07-15 21:48:16 -0700

    format joy also apparently we couldn't fire ISR for non-directional buttons???

--- src/gb_core/joy.cpp
+++ src/gb_core/joy.cpp
@@ -1,12 +1,11 @@
-#include <nowide/iostream.hpp>
-
 #include "gb_core/joy.hpp"
+
+#include <nowide/iostream.hpp>
 
 #include "util/bit.hpp"
 
-Joypad::Joypad(Memory *mem):
-mem(mem) {
-    read_dir_keys = false;
+Joypad::Joypad(Memory *mem): mem(mem) {
+    read_dir_keys    = false;
     read_button_keys = false;
 }
 Joypad::~Joypad() {}
@@ -20,7 +19,7 @@ void Joypad::set_input_state(button_states_t state) {
         }
     }
 
-    if(read_dir_keys) {
+    if(read_button_keys) {
         if(current_state.start || current_state.select || current_state.b || current_state.a) {
             mem->request_interrupt(Memory::Interrupt::JOYPAD_INT);
         }
@@ -34,29 +33,23 @@ Joypad::button_states_t Joypad::get_input_state() {
 u8 Joypad::read() {
     u8 bits = 0;
 
-    if(read_dir_keys)    Bit::set(&bits, 4);
-    if(read_button_keys) Bit::set(&bits, 5);
+    if(read_dir_keys)
+        Bit::set(&bits, 4);
+    if(read_button_keys)
+        Bit::set(&bits, 5);
 
     if(read_dir_keys) {
-        bits |=
-            current_state.down  << 3 |
-            current_state.up    << 2 |
-            current_state.left  << 1 |
-            current_state.right << 0;
+        bits |= current_state.down << 3 | current_state.up << 2 | current_state.left << 1 | current_state.right << 0;
     }
 
     if(read_button_keys) {
-        bits |=
-            current_state.start  << 3 |
-            current_state.select << 2 |
-            current_state.b      << 1 |
-            current_state.a      << 0;
+        bits |= current_state.start << 3 | current_state.select << 2 | current_state.b << 1 | current_state.a << 0;
     }
 
     return ~bits & 0xF;
 }
 
 void Joypad::write(u8 data) {
-    read_dir_keys = Bit::test(data, 5);
+    read_dir_keys    = Bit::test(data, 5);
     read_button_keys = Bit::test(data, 4);
 }
--- src/inc/gb_core/joy.hpp
+++ src/inc/gb_core/joy.hpp
@@ -8,34 +8,35 @@
 
 class Joypad {
 public:
-    struct button_states_t{
-        bool a,b,start,select,up,down,left,right;
-        button_states_t() {
-            a=b=start=select=up=down=left=right=false;
-        }
+    struct button_states_t {
+        bool a, b, start, select, up, down, left, right;
+        button_states_t() { a = b = start = select = up = down = left = right = false; }
         operator std::string() const {
-            return
-                std::string("a: ") + std::to_string(a) +
-                ", b: " + std::to_string(b) +
-                ", start: " + std::to_string(start) +
-                ", select: " + std::to_string(select) +
-                ", up: " + std::to_string(up) +
-                ", down: " + std::to_string(down) +
-                ", left: " + std::to_string(left) +
-                ", right: " + std::to_string(right);
+            // clang-format off
+            return std::string("a: ")
+                 + std::to_string(a)
+                 + ", b: " + std::to_string(b)
+                 + ", start: " + std::to_string(start)
+                 + ", select: " + std::to_string(select)
+                 + ", up: " + std::to_string(up)
+                 + ", down: " + std::to_string(down)
+                 + ", left: " + std::to_string(left)
+                 + ", right: " + std::to_string(right);
+            // clang-format on
         }
     };
 
     Joypad(Memory *mem);
     ~Joypad();
 
-    void set_input_state(button_states_t state);
+    void                    set_input_state(button_states_t state);
     Joypad::button_states_t get_input_state();
 
-    u8 read();
+    u8   read();
     void write(u8 data);
+
 private:
-    bool read_dir_keys, read_button_keys;
+    bool            read_dir_keys, read_button_keys;
     button_states_t current_state;
-    Memory *mem;
+    Memory         *mem;
 };
